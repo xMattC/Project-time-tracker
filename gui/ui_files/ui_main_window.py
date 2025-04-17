beb0c89e0482918efc04001f2f0a22f27bfbfe4d
Matt Corcoran <mkcorcoran2015@gmail.com>
2025-04-17 13:11:38 +0200

    Added two more GUI windows.

--- gui/ui_files/ui_main_window.py
+++ gui/ui_files/ui_main_window.py
@@ -1,4 +1,4 @@
-# Form implementation generated from reading ui file '.\main_window.ui'
+# Form implementation generated from reading ui file 'D:\My Documents\Work\Frelancing\Projects\Project-time-tracker\gui\ui_files\ui_main_window.ui'
 #
 # Created by: PyQt6 UI code generator 6.4.2
 #
